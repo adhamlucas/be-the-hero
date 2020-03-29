959b70d55d9dc49f50253398c562e28332d11988
adhamlucas <adhamlucas20@gmail.com>
2020-03-29 19:13:18 -0400

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+
+import Routes from './routes';
+
+
+//JSX, quando o html está dentro do JavaScript
+//live reload é o p rocesso de carregamento de alterações automáticas do site
+//Componente é um código que isola algo quoda 
+// toda vez que um estado é alterado, as views onde ele está são atualziados
+// Em react nunca podemos alterar o valor de um estado de forma direta, temos que sobrepor
+//  const [counter, setCounter] = useState(0); useSate retorna o valor e uma função que alterar esse estado
+    // Array[valor, funcaoDeAtualizacao]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OminiStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <Routes />
+    );
 }
 
 export default App;
